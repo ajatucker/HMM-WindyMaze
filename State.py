6fc3efb60f16a4b0daaa2feb4f0a7e3b9ed6d052
alexistucker <ajatucker@outlook.com>
2022-03-23 22:49:06 -0400

    Added sensing method

--- State.py
+++ State.py
@@ -11,11 +11,18 @@ class Robot:
     """
    the state represents the robot's current state
     """
-    _my_map = None
+    _my_map = []
 
     _state = None
 
-    _transition_matrix = []
+    _sensor_accuracy = .85
+    """
+                          W N E S
+    """
+    _transition_matrix = [[0,0,0,0],
+                          [0,.1,.1,.1],
+                          [0,0,0,0],
+                          [0,0,0,0]]
 
     def __init__(self, state: State, map) -> None:
         self.my_map = map
@@ -33,18 +40,22 @@ class Robot:
     """
     Sensing calls sensing class
     """
-    def sensing(self):
-        self._state.sensing(self.my_map)
+    def sensing(self, senses):
+        self._state.sensing(self.my_map, senses)
     """
     Moving calls moving class
     """
-    def moving(self):
-        self._state.moving(self.my_map)
+    def moving(self, moves):
+        self._state.moving(self.my_map, moves)
     """
     Filtering will not be called directly, but it can be if needed. Filtering will vary whether the state is moving or sensing
     """
     def filtering(self):
         self._state.filtering(self.my_map)
+    
+    def show(self):
+        rows = ['[' + ','.join(map(lambda x: '{0:.2f}'.format(x),r)) + ']' for r in self._my_map]
+        print('[' + ',\n '.join(rows) + ']')
 
 
 class State(ABC):
@@ -72,29 +83,46 @@ class State(ABC):
 
 
 class SensingState(State):
-    def sensing(self, map) -> None:
-        print("Sensing state handles sensing.")
+    def sensing(self, map, sense) -> None:
+        q = map
+        s = 0.0
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                update = (sense == map[i][j])
+                if(map[i][j] != -1):
+                    map[i][j] = map[i][j] * (update * self.context._sensor_accuracy + (1-update)*(1-self.context._sensor_accuracy))
+                    s += map[i][j]
+
+        # normalize
+        for i in range(len(q)):
+            for j in range(len(map[0])):
+                if(map[i][j] > -1):
+                    map[i][j] /= s
+       # print("Sensing state handles sensing.")
         print("Sensing wants to change the state of the context.")
-        print("We need to filter after sensing.")
+      #print("We need to filter after sensing.")
         self.filtering(map)
+        self.context._my_map = map
         self.context.change_state(MovingState())
 
     def filtering(self, map) -> None:
-        print("Sensing handles filtering request.")
+        #print("Sensing handles filtering request.")
+        pass
     def moving(self, map) -> None:
         pass
 
 
 class MovingState(State):
-    def moving(self, map) -> None:
-        print("Moving state handles moving.")
+    def moving(self, map, move) -> None:
+        #print("Moving state handles moving.")
         print("Moving wants to change the state of the context.")
-        print("We need to filter after moving.")
+        #print("We need to filter after moving.")
         self.filtering(map)
         self.context.change_state(SensingState())
 
     def filtering(self, map) -> None:
-        print("Moving state handles filtering request.")
+        pass
+        #print("Moving state handles filtering request.")
         #self.context.transition_to(ConcreteStateA())
     def sensing(self, map) -> None:
         pass
@@ -127,6 +155,7 @@ if __name__ == "__main__":
             [3.23, 3.23, 3.23, 3.23, -1.0, -1.0, 3.23],
             [3.23, 3.23, 3.23, 3.23, 3.23, 3.23, 3.23]]
     context = Robot(SensingState(), WindMaze)
-    context.sensing()
-    context.moving()
+    context.sensing(sense[1])
+    context.show()
+    context.moving(movements)
 #print (WindMaze)
